b57251c204d31efeb013a571e89a69d4ef273119
Bryce Himebaugh <bhimebau@indiana.edu>
2019-06-07 14:14:35 -0400

    updated makefile

--- e314.mk
+++ e314.mk
@@ -8,8 +8,8 @@ etags:
 
 gdb:
 	cp gdbinit ./build/.gdbinit
-	openocd -f board/st_nucleo_l4.cfg 
-
+#	openocd -s /l/arm2/isetools/tcl -f board/st_nucleo_l4.cfg 
+	openocd -s ~/nobackup/openocd/0.10.0-12-20190422-2015/scripts -f board/st_nucleo_l4.cfg 
 reset:
 	st-flash reset
 
