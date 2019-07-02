1e31b240161970998cd2145b2f95633a1ab4568e
jackclar <jackclar@iu.edu>
2019-07-02 19:58:55 -0400

    added set datetime, date error

--- py_script/test.py
+++ py_script/test.py
@@ -24,8 +24,16 @@ def send(foo):
 		ser.write(str(char).encode('utf-8'))
 		time.sleep(0.2)
 	ser.write(str("\n").encode('utf-8'))
-	
+
+def set_datetime():
+	cur_time = time.localtime()
+	date_code = "ds,{},{},{}".format(cur_time[1], cur_time[2], cur_time[0])
+	time_code = "ts,{},{},{}".format(cur_time[3], cur_time[4], cur_time[5])
+	send(time_code)
+	send(date_code)	
 
 init()
-send("dr")
+set_datetime()
+read()
+get_date()
 read()
