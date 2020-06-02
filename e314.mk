16be2e267f05edb9af6a15c4a558664f5cc17298
Bryce Himebaugh <bhimebau@indiana.edu>
2020-06-02 09:25:38 -0400

    Changed openocd call to be explicit about the cpu and interface.

--- e314.mk
+++ e314.mk
@@ -10,7 +10,9 @@ etags:
 gdb: $(BUILD_DIR)/$(TARGET).elf
 	cp gdbinit ./build/.gdbinit
 #	openocd -s /l/arm2/isetools/tcl -f board/st_nucleo_l4.cfg 
-	openocd -s ~/nobackup/openocd/0.10.0-12-20190422-2015/scripts -f board/st_nucleo_l4.cfg 
+#	openocd -s ~/nobackup/openocd/0.10.0-12-20190422-2015/scripts -f board/st_nucleo_l4.cfg 
+#	openocd -s ~/install_fake/share/openocd/scripts -f board/st_nucleo_l4.cfg
+	openocd -s ~/install_fake/share/openocd/scripts -f interface/stlink.cfg -f target/stm32l4x.cfg
 reset:
 	st-flash reset
 
